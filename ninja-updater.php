d2294a78185e98b090df06a1529361501c99c83b
stingray82 <11146912+stingray82@users.noreply.github.com>
2020-01-31 14:53:38 +0000

    Update ninja-updater.php

--- ninja-updater.php
+++ ninja-updater.php
@@ -3,38 +3,35 @@
  * Plugin name: Ninja-Updater
  * Author: Nathan Foley
  * Author URI: https://github.com/stingray82
- * Version: 0.02
+ * Version: 0.1
  */
-
 require dirname(__FILE__) . '/updates/plugin-update-checker.php';
 Check_Self();
 $next_slugs = get_plugin_update_slugs();
-array_walk($next_slugs,"Check_Others");
-/* allows to print to a log file ---- Delete or close off to view output/*
-$results2 = print_r($next_slugs, true); // $results now contains output from print_r
-file_put_contents(dirname(__FILE__) . '/logs/next_slugs.txt', print_r($next_slugs, true));  
-/* Allows to print to a log file */
+Check_Others();
 
 
-function Check_Others($value,$key){
-    require dirname(__FILE__) . '/updates/plugin-update-checker.php';
-    $location = WP_PLUGIN_DIR."/".$value."/";
-    $location2 =__FILE__;
-    //$results = print_r($location, true); // $results now contains output from print_r
-    //file_put_contents(dirname(__FILE__) . '/logs/location.txt', print_r($location, true));
-    //$results2 = print_r($location2, true); // $results now contains output from print_r
-    //file_put_contents(dirname(__FILE__) . '/logs/location2.txt', print_r($location2, true));  
-    ${"update" . $value} = Puc_v4_Factory::buildUpdateChecker(
-    'http://thedevshed.com/repo/?action=get_metadata&slug='.$value,
-    __FILE__, //Full path to the main plugin file or functions.php.
-    //$location, //Full path to the main plugin file or functions.php.
-    $value
-    );
+
+function Check_Others(){
+    $next_slugs = get_plugin_update_slugs();
+    foreach($next_slugs as $k=>$var){
+        try {
+            extract($var);
+            $key_use = $key_carry;
+            $slug_use = $slug_carry;
+            $update = "" . $slug_use;
+            ${$update} = Puc_v4_Factory::buildUpdateChecker(
+            'http://thedevshed.com/repo/?action=get_metadata&slug=$slug_use',
+            WP_PLUGIN_DIR."/".$key_use, //Full path to the main plugin file or functions.php.
+            $slug_use       
+            );
+        } catch (exception $e) {
+    //code to handle the exception
+        }
+    };
 }
 
-
-
-/* Enable Automatic Updates for the Base Plugin - Ninja-Updater*/
+/* Enable Automatic Updates for the Base Plugin - Ninja-Updater*/ //this is still work in progress
 function auto_update_specific_plugins ( $update, $item ) {
     // Array of plugin slugs to always auto-update
     $plugins = array (      
@@ -52,24 +49,23 @@ function auto_update_specific_plugins ( $update, $item ) {
 function Check_Self() {
     //require dirname(__FILE__) . '/updates/plugin-update-checker.php';
     $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'http://domain.com/repo/?action=get_metadata&slug=ninja-updater',
+    'http://thedevshed.com/repo/?action=get_metadata&slug=ninja-updater',
     __FILE__, //Full path to the main plugin file or functions.php.
     'ninja-updater'
 );
 }
 
-
  function get_plugin_update_slugs(){
     require_once ABSPATH . 'wp-admin/includes/plugin.php';
-    $all_plugins = get_plugins();
+    $plugin_array = get_plugins();
     if (empty($plugin_array)) {return array();}
     $results2 = print_r($plugin_array, true); // $results now contains output from print_r
-    file_put_contents(dirname(__FILE__) . '/logs/plugin_array.txt', print_r($plugin_array, true));
-    $ar = array_column($plugin_array, 0);
-    foreach($all_plugins as $key => $plugin){
+    //$ar = array_column($plugin_array, 0);
+    $plugnsX = array();
+    foreach($plugin_array as $key => $plugin){
         $key_carry = $key;
         $slug_carry = sanitize_title($plugin['Name']);
-        $plugnsX = compact ("key_carry","slug_carry");
+        $plugnsX[] = compact ("key_carry","slug_carry");
     }
     return $plugnsX;
  }
