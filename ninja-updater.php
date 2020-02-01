3e06095655fa470788f9d5da610bb85cb3ac4df0
stingray82 <11146912+stingray82@users.noreply.github.com>
2020-02-01 16:54:37 +0000

    Push to 0.1.2
    It now does themes, and plugins directly without modification to the plugins, there are some exceptions such as wp-smush-pro and I'd guess other WPMU DEV projects and other providers
    
    wp-smush-pro fix - just remove the WPMU DEV dashboard and then rename or remove plugins/wp-smush-pro/core/external/dash-notice/wpmudev-dash-notification.php
    
    Once installed and activated you should be able to check for updates in the plugin screen as it adds a check plugin option for all plugins and you can also check all by going to the WordPress updates ('https://yoursite.co.uk/wp-admin/update-core.php'
    
    I've made it auto-updating from your server repository, I haven't checked this works but it should do, I haven't added this to pull from WordPress as you never know what happens.

--- ninja-updater.php
+++ ninja-updater.php
@@ -2,14 +2,97 @@
 /*
  * Plugin name: Ninja-Updater
  * Author: Nathan Foley
- * Author URI: https://github.com/stingray82
- * Version: 0.01
+ * Author URI: https://github.com/stingray82 / https://www.wordpressninja.co.uk
+ * Version: 0.1.2
  */
 
-/* Check For Updates */
+// This works with The location of your ZIP File repo using: https://github.com/YahnisElsts/wp-update-server
+// Themes - Edit Style.css in base directory with your new version number and upload to your repo
+// Plugins -Edit Main plugin file with your new version number and upload to your repo
+
+// No need to edit below this line //
 require dirname(__FILE__) . '/updates/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'location to github will evenutally go here',
-	__FILE__, //Full path to the main plugin file or functions.php.
-	'ninja-updater'
-);
+add_filter( 'auto_update_plugin', 'auto_update_specific_plugins', 10, 2 );
+Check_Plugins();
+Check_Themes();
+
+function Check_Plugins(){
+    $next_slugs = get_plugin_update_slugs();
+    foreach($next_slugs as $k=>$var){
+        try {
+            extract($var);
+            $key_use = $key_carry;
+            $slug_use = $slug_carry;
+            $update = rand() . $slug_use;
+            ${$update} = Puc_v4_Factory::buildUpdateChecker(
+            'Your_Server_Goes_Here'.$slug_use,
+            WP_PLUGIN_DIR."/".$key_use, //Full path to the main plugin file or functions.php.
+            $slug_use       
+            );
+        } catch (exception $e) {
+    //code to handle the exception
+        }
+    };
+}
+
+function Check_Themes(){
+    $next_slugs = get_themes_update_slugs();
+    foreach($next_slugs as $k=>$var){
+        try {
+            extract($var);
+            $theme = $find_theme;
+            $update = rand() . $theme;
+            ${$update} = Puc_v4_Factory::buildUpdateChecker(
+            'Your_Server_Goes_Here'.$theme,
+            get_theme_root()."/".$theme, //Full path to the main plugin file or functions.php.
+            $theme       
+            );
+        } catch (exception $e) {
+    //code to handle the exception
+        }
+    };
+}
+
+function auto_update_specific_plugins ( $update, $item ) {
+    $plugins = array ( // Plugins to  auto-update
+        'ninja-updater',
+    );
+    if ( in_array( $item->slug, $plugins ) )
+        return true; // Auto-update specified plugins
+    else return false; // Don't auto-update all other plugins
+}
+
+
+ function get_plugin_update_slugs(){
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+    $plugin_array = get_plugins();
+    if (empty($plugin_array)) {return array();}
+    $results2 = print_r($plugin_array, true); // $results now contains output from print_r
+    //$ar = array_column($plugin_array, 0);
+    $plugnsX = array();
+    foreach($plugin_array as $key => $plugin){
+        $key_carry = $key;
+        $slug_carry = sanitize_title($plugin['Name']);
+        $plugnsX[] = compact ("key_carry","slug_carry");
+    }
+    return $plugnsX;
+ }
+
+ function get_themes_update_slugs(){
+    require_once ABSPATH . 'wp-includes/theme.php';
+    $theme_array = wp_get_themes();
+    file_put_contents(dirname(__FILE__) . '/logs/theme_array.txt', print_r($theme_array, true));
+    if (empty($theme_array)) {return array();}
+    $results2 = print_r($theme_array, true); // $results now contains output from print_r
+    $themeX = array();
+    foreach ($theme_array as $theme_array) {
+        # code...
+          
+        foreach($theme_array as $key => $theme){
+            $find_theme = $theme_array->get( 'TextDomain' );
+            $themeX[] = compact ("find_theme");         
+        }
+        
+    }
+    return $themeX;
+}
